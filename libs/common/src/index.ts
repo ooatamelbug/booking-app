c2a756740b053b9b53882192d275b8b42f6362f0
ooatamelbug <mohamedwork636@gmail.com>
2022-09-12 22:36:13 +0200

    create new app shops

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,3 @@
-export * from './common.module';
-export * from './common.service';
+export * from './database/database.module';
+export * from './rmq/rmq.module';
+export * from './rmq/rmq.service';
